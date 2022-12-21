7f9a3e7040a3f002fb98b8d5dfa9dbb1e14f1e46
fuji37450 <40747030s@ntnu.edu.tw>
2022-12-21 16:07:42 +0800

    feat(find_keypoints): enable specify thread num

--- examples/find_keypoints.cpp
+++ examples/find_keypoints.cpp
@@ -1,5 +1,6 @@
 #include <iostream> 
 #include <string>
+#include <omp.h>
 
 #include "image.hpp"
 #include "sift.hpp"
@@ -9,10 +10,23 @@ int main(int argc, char *argv[])
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
 
-    if (argc != 2) {
+    if (argc < 2) {
         std::cerr << "Usage: ./find_keypoints input.jpg (or .png)\n";
         return 0;
     }
+
+    int num_thread = -1;
+    int max_threads = omp_get_max_threads();
+
+    if (argc == 3) {
+        num_thread = std::min(atoi(argv[2]), max_threads);
+    }
+    else {
+        num_thread = max_threads;
+    }
+
+    omp_set_num_threads(num_thread);
+
     Image img(argv[1]);
     img =  img.channels == 1 ? img : rgb_to_grayscale(img);
 
@@ -20,6 +34,7 @@ int main(int argc, char *argv[])
     Image result = sift::draw_keypoints(img, kps);
     result.save("result.jpg");
 
+    std::cout << "Thread count: " << num_thread << "\n";
     std::cout << "Found " << kps.size() << " keypoints. Output image is saved as result.jpg\n";
     return 0;
 }
